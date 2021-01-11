848c03f6729da36acccb8ed996dda7531bfb2901
jihongBae <dasog94@gmail.com>
2021-01-11 22:51:24 +0900

    build gradle
    build tha base

---
+++ src/main/java/hello/spring_based_on_raw_java/SpringBasedOnRawJavaApplication.java
@@ -0,0 +1,13 @@
+package hello.spring_based_on_raw_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasedOnRawJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasedOnRawJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/spring_based_on_raw_java/SpringBasedOnRawJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spring_based_on_raw_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasedOnRawJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
